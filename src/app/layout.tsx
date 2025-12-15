12546c174ae20a01d138275eaec51347e49d4293
Hashversion <110537156+Hashversion@users.noreply.github.com>
2025-12-15 23:54:41 +0530

    feat: enhance metadata structure and add viewport settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "@/styles/global.css";
-import { Metadata } from "next";
+import { type Metadata, type Viewport } from "next";
 import { domAnimation, LazyMotion } from "motion/react";
 import * as m from "motion/react-m";
 import { Banner } from "fumadocs-ui/components/banner";
 import { fontsVariable } from "@/styles/fonts";
 
 export const metadata: Metadata = {
-  title: "Vazen",
+  title: {
+    template: "&s :: Vazen",
+    default: "Vazen",
+  },
   description: "A production grade monorepo starter to build full-stack applications on cloudflare",
   icons: {
     icon: [
@@ -24,6 +27,13 @@ export const metadata: Metadata = {
     ],
   },
   metadataBase: new URL("https://vazen.dev"),
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#000" },
+    { media: "(prefers-color-scheme: light)", color: "#fff" },
+  ],
 };
 
 const isProduction = process.env.NODE_ENV === "production";
