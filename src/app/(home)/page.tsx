2f7cb7a3b70dfccee4266f09db85212d018cb94a
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-02-18 15:50:29 +0530

    feat: enhance homepage layout with new SVG pattern section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,31 @@
 import { CopyCodeBlock } from "@/components/ui/copy-code-block";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <section className="p-12">
-      <div className="max-w-340 mx-auto">
-        <div className="max-w-prose space-y-5">
-          <h1 className="text-2xl text-balance font-medium text-start">
-            A production-grade monorepo starter for building full-stack applications on cloudflare
-          </h1>
-          <CopyCodeBlock value="npx degit Hashversion/vazen" />
+    <>
+      <section className="p-12">
+        <div className="max-w-340 mx-auto">
+          <div className="max-w-prose space-y-5">
+            <h1 className="text-2xl text-balance font-medium text-start">
+              A production-grade monorepo starter for building full-stack applications on cloudflare
+            </h1>
+            <CopyCodeBlock value="npx degit Hashversion/vazen" />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="p-3">
+        <div className="max-w-340 mx-auto">
+          <div className="w-full h-60 relative overflow-hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="896">
+              <pattern id="pattern-circles" x="0" y="0" width="40" height="40" patternUnits="userSpaceOnUse">
+                <circle cx="20" cy="20" r="20" fill="currentColor"></circle>
+              </pattern>
+              <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-circles)"></rect>
+            </svg>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
