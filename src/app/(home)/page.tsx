80fc15e6162d6284bbd929328afc3f865e600faf
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-02-23 23:10:14 +0530

    fix: change CirclePattern SVG dot fill color to use currentColor

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -70,7 +70,7 @@ function CirclePattern() {
       <svg width="100%" height="100%" viewBox="0 0 1320 200" xmlns="http://www.w3.org/2000/svg">
         <defs>
           <pattern id="dots" width="40" height="40" patternUnits="userSpaceOnUse">
-            <circle cx="20" cy="20" r="20" fill="black" />
+            <circle cx="20" cy="20" r="20" fill="currentColor" />
           </pattern>
         </defs>
 
